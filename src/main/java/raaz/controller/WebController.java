d20bf47399c0625024a20ec9b846026c4fc92d51
Amy Miles <33338870+amy-miles@users.noreply.github.com>
2023-11-24 16:59:51 -0600

    added the following 1. navigation methods in WebController 2. static and templates folder in src/main/resources 3. images folder with 3 images in static folder 4. fragments folder in templates folder and header.html 5. index,html, about.html, samplePage.html, and sampleTable.html

--- src/main/java/raaz/controller/WebController.java
+++ src/main/java/raaz/controller/WebController.java
@@ -1,10 +1,40 @@
 package raaz.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
 /**
  * @author torirem - aaustin5
  * CIS175 - Fall 2023
  * Nov 7, 2023
  */
-public class WebController {
 
+/**
+ * @author Amy Miles
+ * CIS 175 - Fall 2023
+ * Nov 24, 2023
+ */
+
+@Controller
+public class WebController {
+ 
+	@GetMapping("/")
+	public String openIndex() {
+		return "index";
+	}	
+	
+    @GetMapping("/sampleTable")
+    public String openSampleTable() {
+        return "sampleTable"; 
+    }
+    
+    @GetMapping("/samplePage")
+    public String openSamplePage() {
+        return "samplePage"; 
+    }
+    
+    @GetMapping("/about")
+    public String openAbout(){
+    	return "about";
+    }
 }
