40d8c2c6ba64e9b83a14f661e569735b6cc0dba9
Amy Miles <33338870+amy-miles@users.noreply.github.com>
2023-11-24 16:37:33 -0600

    Added the following 1. methods in WebController for navbar links 2. templates folder -> fragments folder and header.html 3. static folder -> images folder with 3 images 4. in templates about.html, index.html 5. added samplePage and sampleTable to show style layout

--- src/main/java/raaz/controller/WebController.java
+++ src/main/java/raaz/controller/WebController.java
@@ -1,10 +1,38 @@
 package raaz.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
 /**
  * @author torirem - aaustin5
  * CIS175 - Fall 2023
  * Nov 7, 2023
  */
+/**
+ * @author Amy Miles
+ * CIS 175 - Fall 2023
+ * Nov 24, 2023
+ */
+@Controller
 public class WebController {
-
+	//mapping for navigation (navbar in header.html in fragments folder)
+	@GetMapping("/")
+	public String openIndex() {
+		return "index";
+	}	
+	
+    @GetMapping("/about")
+    public String openAbout(){
+    	return "about";
+    }
+	
+    @GetMapping("/samplePage")
+    public String openSamplePage() {
+        return "samplePage"; 
+    }
+    
+    @GetMapping("/sampleTable")
+    public String openSampleTable() {
+    	return "sampleTable";
+    }
 }
