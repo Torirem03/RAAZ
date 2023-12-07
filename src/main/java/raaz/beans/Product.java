1817566952b044c34f687b13126921ad2e08773c
Rachel Schulz <rachelsbeckwith@gmail.com>
2023-12-07 15:50:32 -0600

    in Product, fixed generated id

--- src/main/java/raaz/beans/Product.java
+++ src/main/java/raaz/beans/Product.java
@@ -8,6 +8,7 @@ package raaz.beans;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -26,7 +27,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Product {
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private long productId;
 	private String productName;
 	private String description;
